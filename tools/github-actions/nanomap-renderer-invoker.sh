9f79e0ae0ac4754f4cc7a7fa96d40609df21d83c
GremlingSS <68038883+GremlingSS@users.noreply.github.com>
2021-09-01 20:58:32 +0100

    Forgot to rename a lot of it oof

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -13,9 +13,9 @@ tools/github-actions/nanomap-renderer minimap -w 2240 -h 2240 "${map_files[@]}"
 cd "data/nanomaps"
 
 mv "triumph-01-deck1_nanomap_z1.png" "triumph_nanomap_z1.png"
-mv "triumph-01-deck1_nanomap_z1.png" "triumph_nanomap_z2.png"
-mv "triumph-01-deck1_nanomap_z1.png" "triumph_nanomap_z3.png"
-mv "triumph-01-deck1_nanomap_z1.png" "triumph_nanomap_z4.png"
+mv "triumph-02-deck2_nanomap_z2.png" "triumph_nanomap_z2.png"
+mv "triumph-03-deck3_nanomap_z3.png" "triumph_nanomap_z3.png"
+mv "triumph-04-deck4_nanomap_z4.png" "triumph_nanomap_z4.png"
 
 cd "../../"
 cp data/nanomaps/* "icons/_nanomaps/"
