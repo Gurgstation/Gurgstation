ae0fd70b4bf1459cf68b0e2cd08c4c165ddfb32c
Sym <gamer2418@hotmail.com>
2023-06-21 02:45:43 -0400

    Hold onto your pants
    This might cause TGUI errors until I re-build the bundle. Don't ask why im not staging the bundle with this commit.

--- tgui/packages/tgui/interfaces/VorePanel.js
+++ tgui/packages/tgui/interfaces/VorePanel.js
@@ -868,6 +868,7 @@ const VoreUserPreferences = (props, context) => {
     show_vore_fx,
     can_be_drop_prey,
     can_be_drop_pred,
+    allow_inbelly_spawning,
     allow_spontaneous_tf,
     step_mechanics_active,
     pickup_mechanics_active,
@@ -1053,6 +1054,21 @@ const VoreUserPreferences = (props, context) => {
         disabled: 'Throw Vore Disabled',
       },
     },
+    inbelly_spawning: {
+      action: 'toggle_allow_inbelly_spawning',
+      test: allow_inbelly_spawning,
+      tooltip: {
+        main:
+          'This toggle is ghosts being able to spawn in one of your bellies.' +
+          ' You will have to confirm again when they attempt to.',
+        enable: 'Click here to allow prey to spawn in you.',
+        disable: 'Click here to prevent prey from spawning in you.',
+      },
+      content: {
+        enabled: 'Inbelly Spawning Allowed',
+        disabled: 'Inbelly Spawning Forbidden',
+      },
+    },
     noisy: {
       action: 'toggle_noisy',
       test: noisy,
@@ -1229,6 +1245,9 @@ const VoreUserPreferences = (props, context) => {
         </Flex.Item>
         <Flex.Item basis="32%">
           <VoreUserPreferenceItem spec={preferences.toggle_throw_vore} />
+        </Flex.Item>
+        <Flex.Item basis="32%">
+          <VoreUserPreferenceItem spec={preferences.inbelly_spawning} />
         </Flex.Item>
         <Flex.Item basis="32%" grow={1}>
           <VoreUserPreferenceItem spec={preferences.noisy} />
